24373259ab6a1da3a5bf8665f3012a58f1f1a32f
AND1993 <andre_fonsecasantos@hotmail.com>
2023-11-28 15:16:20 +0000

    new file: controles_customizados.html new file: controles_customizados.js

---
+++ projeto-log/Maps/Exemplo_1/controles/controles_customizados.js
@@ -0,0 +1,14 @@
+
+//Variavel
+var map;
+//função do mapa
+function initMap() {
+    //Opções do mapa
+    var mapOptions = {
+        //Centralização do mapa
+        center: { lat: -3.716816, lng: -38.519115 },
+        zoom: 8,
+    };
+
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+}
