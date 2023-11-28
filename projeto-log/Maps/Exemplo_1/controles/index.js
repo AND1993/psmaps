36922bfab21b6de47d09c38a82b334087f0ff6fd
AND1993 <andre_fonsecasantos@hotmail.com>
2023-11-28 15:08:23 +0000

    MODIFIED: index.js

--- projeto-log/Maps/Exemplo_1/controles/index.js
+++ projeto-log/Maps/Exemplo_1/controles/index.js
@@ -8,7 +8,27 @@ function initMap() {
         //Centralização do mapa
         center: { lat: -3.716816, lng: -38.519115 },
         zoom: 8,
-        disableDefaultUI: false
+        //disableDefaultUI: true // Desabilita todos os icones do google maps
+       // zoomControl: false, // desabilita o controle de zoom
+        //streetViewControl: false, // desabilita o street View (bonequinho)
+        //mapTypeControl: false, // desabilita o Tipo de mapa
+        //scaleControl: true, // abilita o controle de escala (desabilitado por padrão)
+       // rotateControl: false, // desabilita o controle de rotação de tela
+       //fullscreenControl: false // desabilita o botão de tela cheia
+       
+       //-------------Personalizar tipos de mapa
+       mapTypeControlOptions:{
+        //o tipo de menu
+        //style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR, //Menu horizontal
+        style: google.maps.MapTypeControlStyle.DROPDOWN_MENU, // Menu suspenso (você clica e seleciona o tipo do mapa)
+        position: google.maps.ControlPosition.TOP_CENTER // posiciona o menu de mapas no centro da tela
+        //Para mais opções de posicionamento, altere 'TOP_CENTER' para uma das opções "contantes ControlPosition" no site https://developers.google.com/maps/documentation/javascript/reference/control?hl=pt-br#ControlPosition
+       },
+       //--------Personalizar Full Screen (tela cheia)
+       //Só permite selecionar o posicionamento do icone
+       fullscreenControlOptions:{
+        position: google.maps.ControlPosition.LEFT_TOP
+       }
     };
 
     map = new google.maps.Map(document.getElementById('map'), mapOptions);
